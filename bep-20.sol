aeea3a6c77a65c9105f7b63a4402fdd28d33cd70
BurnedOx <makhammer8@gmail.com>
2021-12-07 14:04:25 +0530

    tttt

--- bep-20.sol
+++ bep-20.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "./ERC20.sol";
 
 contract CentumoToken is ERC20 {
-    constructor(uint256 initialSupply) public ERC20 ("CENTUMO TOKEN", "CENT") {
+    constructor(uint256 initialSupply) public ERC20("CENTUMO TOKEN", "CENT") {
         _mint(msg.sender, initialSupply);
     }
 }
